8c74565f379120a2214011a342476396c5978f4e
“github.com/josebatres301-dot” <josebatres301@gmail.com>
2026-04-21 22:25:42 -0500

    move firebase config to env variables

--- src/firebase.js
+++ src/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDFBgSUsH1zX9dRJNiDcBgQm8g3-Em3D1c",
-  authDomain: "jcr-flooring.firebaseapp.com",
-  projectId: "jcr-flooring",
-  storageBucket: "jcr-flooring.firebasestorage.app",
-  messagingSenderId: "674973259143",
-  appId: "1:674973259143:web:4749ea6d8499ed9dd02f22"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = initializeApp(firebaseConfig);
